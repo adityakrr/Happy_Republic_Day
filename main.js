eb778db5f102bb3c72c1f5b70f362b31dcc3e002
Aditya Kumar <109237407+adityakrr@users.noreply.github.com>
2024-01-26 00:53:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ window.onload = function() {
         document.querySelector('.loader_container').style.display = "none";
         if (counter == 0){
             Swal.fire({
-                html: `Are you ready to go ? ✨<marquee>👉Developed by Aditya💖</marquee>`
+                html: `Are you ready to go ? ✨<marquee>Developed by Aditya</marquee>`
             });
         }
         document.body.addEventListener('click',function(){
